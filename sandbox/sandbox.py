fc421fcfb0f1981f545f1964df119eb0ae1f3274
lllangWV <lllangWV@gmail.com>
2024-02-06 11:20:32 -0500

    Added CDVAE and updated env.yml file

---
+++ sandbox/sandbox.py
@@ -0,0 +1,12 @@
+import torch
+import torch
+import torch_geometric
+from torch_geometric.datasets import TUDataset
+import networkx as nx
+
+
+print(torch.__version__)
+l= [1,2,3,4,5]
+print(torch.cuda.is_available())
+
+
